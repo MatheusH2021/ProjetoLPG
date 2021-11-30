d93976f3a6bcc49d0d1d8208773bda222d4047bc
MatheusH2021 <matheus.henrique.mhms@gmail.com>
2021-11-30 11:20:04 -0300

    Atualizado

--- ProjetoLPG/settings.py
+++ ProjetoLPG/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-&&hu6ldg878yce4y56d!%9%)-7*hg6gdzgas4be$ydt6j5qk(x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
